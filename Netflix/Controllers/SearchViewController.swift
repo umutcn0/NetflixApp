f7aabb985512ed163bca3679468da752f9dbb7a9
Umut Can <game.mudur@hotmail.com>
2022-08-17 01:31:47 +0300

    Search Page Update

--- Netflix/Controllers/SearchViewController.swift
+++ Netflix/Controllers/SearchViewController.swift
@@ -8,13 +8,65 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private var titles : [Title] = [Title]()
+    
+    private let searchTableView : UITableView = {
+        let table = UITableView()
+        table.register(ComingTableViewCell.self, forCellReuseIdentifier: ComingTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
-
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.topItem?.largeTitleDisplayMode = .always
+        view.addSubview(searchTableView)
+        fetchDiscoverMovies()
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchTableView.frame = view.bounds
+    }
+    
+    private func fetchDiscoverMovies(){
+        APICaller.shared.getDiscoverMovies { [weak self]result in
+            switch result{
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.searchTableView.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier:ComingTableViewCell.identifier , for: indexPath) as? ComingTableViewCell else {
+            return UITableViewCell()
+            }
+        cell.configure(with: TitleViewModel(posterURL: titles[indexPath.row].poster_path ?? "", titleName: titles[indexPath.row].original_title ?? titles[indexPath.row].original_name ?? "Unknown"))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
 }
